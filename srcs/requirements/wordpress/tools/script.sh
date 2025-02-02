ce74df0b69a4fc14a262f5cd89ce2d8c4c7978c6
Emu Kamada <es377233@gmail.com>
2025-02-02 18:19:06 +0900

    fix: secretの設定

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -12,16 +12,15 @@ if [ ! -f wp-config.php ]; then
 fi
 
 if [ ! -f wp-config.php ]; then
-    wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --dbprefix=$DB_PREFIX --allow-root
-    echo -e "<?php\nif (!isset(\$_SERVER['HTTP_HOST'])) {\n    \$_SERVER['HTTP_HOST'] = $DOMAIN_NAME;\n}\n?>" >> wp-config.php
+    wp config create --dbname=$(cat $DB_NAME_FILE) --dbuser=$(cat $DB_USER_FILE) --dbpass=$(cat $DB_PASSWORD_FILE) --dbhost=$(cat $DB_HOST_FILE) --dbprefix=$(cat $DB_PREFIX_FILE) --allow-root
 fi
 
 if [ ! $(wp core is-installed --allow-root)]; then
-    wp core install --url=$DOMAIN_NAME --title=inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+    wp core install --url=$DOMAIN_NAME --title=inception --admin_user=$(cat $WP_ADMIN_USER_FILE) --admin_password=$(cat $WP_ADMIN_PASSWORD_FILE) --admin_email=$(cat $WP_ADMIN_EMAIL_FILE) --allow-root
 fi
 
-if ! wp user get $WP_AUTHOR_USER --allow-root > /dev/null 2>&1; then
-    wp user create $WP_AUTHOR_USER $WP_AUTHOR_EMAIL --role=author --user_pass=$WP_AUTHOR_PASSWORD --allow-root
+if ! wp user get $(cat $WP_AUTHOR_USER_FILE) --allow-root > /dev/null 2>&1; then
+    wp user create $(cat $WP_AUTHOR_USER_FILE) $(cat $WP_AUTHOR_EMAIL_FILE) --role=author --user_pass=$(cat $WP_AUTHOR_PASSWORD_FILE) --allow-root
 fi
 
 
