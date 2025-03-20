81daa2bce7e85d22deebf6cb8031193b6701e441
soragogo <es377233@gmail.com>
2025-03-20 13:23:26 +0900

    space

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -15,7 +15,7 @@ if [ ! -f wp-config.php ]; then
     wp config create --dbname=$(cat $DB_NAME_FILE) --dbuser=$(cat $DB_USER_FILE) --dbpass=$(cat $DB_PASSWORD_FILE) --dbhost=$(cat $DB_HOST_FILE) --dbprefix=$(cat $DB_PREFIX_FILE) --allow-root
 fi
 
-if [ ! $(wp core is-installed --allow-root)]; then
+if [ ! $(wp core is-installed --allow-root) ]; then
     wp core install --url=$DOMAIN_NAME --title=inception --admin_user=$WP_ADMIN_USER --admin_password=$(cat $WP_ADMIN_PASSWORD_FILE) --admin_email=$(cat $WP_ADMIN_EMAIL_FILE) --allow-root
 fi
 
